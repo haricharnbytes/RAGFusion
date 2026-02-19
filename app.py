ada2559f64c7c80f9fb51bb795f8f11650521276
haricharanbytes <hccharankatta@gmail.com>
2026-02-19 12:04:17 +0530

    import modules

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+import base64
+from typing import List, Dict, Any
+import uuid
+import hashlib
+
+# LangChain imports for 2025 updates
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage
+from langchain_core.vectorstores import InMemoryVectorStore
